c1f2c0797d232f08446d5cbec25fa3e649f00332
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2019-01-28 02:59:25 +0200

    release

--- src/main/java/com/mkyong/jms/routes/QueueRoutes.java
+++ src/main/java/com/mkyong/jms/routes/QueueRoutes.java
@@ -1,6 +1,7 @@
 package com.mkyong.jms.routes;
 
 import com.mkyong.jms.processor.Test1Processor;
+import com.mkyong.jms.processor.Test2Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,10 +17,17 @@ public class QueueRoutes extends RouteBuilder {
 	@Autowired
 	private Test1Processor test1Processor;
 
+	@Autowired
+	private Test2Processor test2Processor;
+
 	@Override
 	public void configure() throws Exception {
 		from(JMS + ":test1:queue?concurrentConsumers=" + concurrentConsumers)
 				.routeId("test1")
 				.process(test1Processor);
+
+		from(JMS + ":test2:queue?concurrentConsumers=" + concurrentConsumers)
+				.routeId("test2")
+				.process(test2Processor);
 	}
 }
