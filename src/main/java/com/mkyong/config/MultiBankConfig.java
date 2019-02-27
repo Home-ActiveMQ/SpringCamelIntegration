7d7a395a04d2d071dca375288b9dcdd57b5bef14
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-02-27 18:31:16 +0200

    #multibank

--- src/main/java/com/mkyong/config/MultiBankConfig.java
+++ src/main/java/com/mkyong/config/MultiBankConfig.java
@@ -6,12 +6,16 @@ import com.mkyong.service.BankService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 
 /**
  * @see https://stackoverflow.com/questions/41025627/how-can-i-name-a-service-with-multiple-names-in-spring
+ * @see https://stackoverflow.com/questions/4540713/add-bean-programmatically-to-spring-web-app-context
  *
  * https://dbdiagram.io/d/5c6ef578f7c5bb70c72f15f6
  */
@@ -23,17 +27,21 @@ public class MultiBankConfig {
     @Autowired
     private BankService bankService;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @PostConstruct
     public void init() {
-        LOGGER.debug(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
-        for (Bank bank: bankService.findAll()) {
-            LOGGER.debug(bank);
-        }
-        LOGGER.debug(" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ");
+        ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) applicationContext).getBeanFactory();
+
+//        beanFactory.registerSingleton("test1Processor", TestProcessor.class);
+//        beanFactory.registerSingleton("test2Processor", TestProcessor.class);
+//        beanFactory.registerSingleton("test3Processor", TestProcessor.class);
+        for (Bank bank: bankService.findAll()) beanFactory.registerSingleton(bank.getUniqueUrl(), TestProcessor.class);
     }
 
-    @Bean(name = {"test1Processor", "test2Processor", "test3Processor"})
-    public TestProcessor createTestProcessor(@Autowired TestProcessor testProcessor) {
-        return testProcessor;
-    }
+//    @Bean(name = { "test1Processor", "test2Processor", "test3Processor" })
+//    public TestProcessor createTestProcessor(@Autowired TestProcessor testProcessor) {
+//        return testProcessor;
+//    }
 }
