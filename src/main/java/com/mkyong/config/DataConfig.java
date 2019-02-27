1f63448b9afc8c326ceb7b6b8f675a4f0e2a1592
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-02-27 17:48:59 +0200

    #multibank

--- src/main/java/com/mkyong/config/DataConfig.java
+++ src/main/java/com/mkyong/config/DataConfig.java
@@ -1,14 +1,13 @@
 package com.mkyong.config;
 
 import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -17,25 +16,34 @@ import java.util.Properties;
 @EnableJpaRepositories("com.mkyong.repository")
 public class DataConfig {
 
-    private static final String PROP_DATABASE_DRIVER = "spring.datasource.driver-class-name";
-    private static final String PROP_DATABASE_PASSWORD = "spring.datasource.password";
-    private static final String PROP_DATABASE_URL = "spring.datasource.url";
-    private static final String PROP_DATABASE_USERNAME = "spring.datasource.username";
-    private static final String PROP_HIBERNATE_DIALECT = "spring.jpa.properties.hibernate.dialect";
-    private static final String PROP_HIBERNATE_SHOW_SQL = "spring.jpa.show-sql";
-    private static final String PROP_HIBERNATE_HBM2DDL_AUTO = "spring.jpa.hibernate.ddl-auto";
-    private static final String PROP_ENTITYMANAGER_PACKAGES_TO_SCAN = "data.entitymanager.packages.to.scan";
+    private final String ENTITYMANAGER_PACKAGES_TO_SCAN = "com.mkyong.entity";
 
-    @Resource
-    private Environment env;
+    @Value("${spring.datasource.url:}")
+    private String PROP_DATABASE_URL;
 
+    @Value("${spring.datasource.username:}")
+    private String PROP_DATABASE_USERNAME;
 
+    @Value("${spring.datasource.password:}")
+    private String PROP_DATABASE_PASSWORD;
+
+    @Value("${spring.datasource.driver-class-name:}")
+    private String PROP_DATABASE_DRIVER;
+
+    @Value("${spring.jpa.show-sql:}")
+    private String PROP_HIBERNATE_SHOW_SQL;
+
+    @Value("${spring.jpa.hibernate.ddl-auto:}")
+    private String PROP_HIBERNATE_HBM2DDL_AUTO;
+
+    @Value("${spring.jpa.properties.hibernate.dialect:}")
+    private String PROP_HIBERNATE_DIALECT;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN));
+        entityManagerFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
         entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
         entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
         return entityManagerFactoryBean;
@@ -44,18 +52,18 @@ public class DataConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getRequiredProperty(PROP_DATABASE_DRIVER));
-        dataSource.setUrl(env.getRequiredProperty(PROP_DATABASE_URL));
-        dataSource.setUsername(env.getRequiredProperty(PROP_DATABASE_USERNAME));
-        dataSource.setPassword(env.getRequiredProperty(PROP_DATABASE_PASSWORD));
+        dataSource.setDriverClassName(PROP_DATABASE_DRIVER);
+        dataSource.setUrl(PROP_DATABASE_URL);
+        dataSource.setUsername(PROP_DATABASE_USERNAME);
+        dataSource.setPassword(PROP_DATABASE_PASSWORD);
         return dataSource;
     }
 
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
-        properties.put(PROP_HIBERNATE_DIALECT, env.getRequiredProperty(PROP_HIBERNATE_DIALECT));
-        properties.put(PROP_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROP_HIBERNATE_SHOW_SQL));
-        properties.put(PROP_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROP_HIBERNATE_HBM2DDL_AUTO));
+        properties.put(PROP_HIBERNATE_DIALECT, PROP_HIBERNATE_DIALECT);
+        properties.put(PROP_HIBERNATE_SHOW_SQL, PROP_HIBERNATE_SHOW_SQL);
+        properties.put(PROP_HIBERNATE_HBM2DDL_AUTO, PROP_HIBERNATE_HBM2DDL_AUTO);
         return properties;
     }
 }
